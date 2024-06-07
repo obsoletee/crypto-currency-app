7d0cff63c45c34eab6b970d60958db8ca3ae10d9
user <idonotwanttostudy@mail.ru>
2024-06-07 12:53:09 +0300

    Added header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import '../css/header.css'
-import logo from "/btc2xicon.png"
+import styles from './header.module.scss';
+import logo from "/btc2xicon.png";
 
 export default function Header() {
 	return (
-		<header className='header'>
-			<div className="header__container">
-				<div className="container__info">
-					<div className='container__info__title'>CryptoApp</div>
-					<div className="container__info__logo"><img src={logo} alt="logo"/></div>
+		<header className={styles.header}>
+			<div className={styles.container}>
+				<div className={styles.info}>
+					<div className={styles.title}>CryptoApp</div>
+					<div className={styles.logo}><img src={logo} alt="logo"/></div>
 				</div>
-				<div className="container__crypto">
-					<div className="container__crypto__elem">BTC</div>
-					<div className="container__crypto__elem">ETH</div>
-					<div className="container__crypto__elem">BNB</div>
+				<div className={styles.crypto}>
+					<div className={styles.elem}>BTC</div>
+					<div className={styles.elem}>ETH</div>
+					<div className={styles.elem}>BNB</div>
 				</div>
 			</div>
 		</header>
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './css/index.css'
+import './scss/null.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
